129709514b103977cb6aba5b8bebbf367206faba
Ivan Tikhonov <itikhonov@mail.com>
2024-11-07 16:46:49 +0300

    move IndexOfPart

--- IT.Json.Tests/EnumJsonConverterTest.cs
+++ IT.Json.Tests/EnumJsonConverterTest.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using IT.Json.Internal;
 
 namespace IT.Json.Tests;
 
@@ -69,59 +70,6 @@ enum EnumEscaped
 {
     [JsonPropertyName("\"Escaped\"")]
     Escaped = 0
-}
-
-public static class Ext
-{
-    internal static int IndexOfPart<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> value, out int length)
-        where T : IEquatable<T>?
-    {
-        if (span.Length == 0) throw new ArgumentException("span is empty", nameof(span));
-        
-        var maxLength = value.Length;
-        if (maxLength == 0) throw new ArgumentException("value is empty", nameof(value));
-        if (maxLength == 1)
-        {
-            var idx = span.IndexOf(value);
-            length = idx == -1 ? 0 : 1;
-            return idx;
-        }
-
-        var index = -1;
-        var len = 0;
-        var v = value[0];
-        for (int i = 0; i < span.Length; i++)
-        {
-            var s = span[i];
-            if (EqualityComparer<T>.Default.Equals(v, s))
-            {
-                if (index == -1) index = i;
-                if (++len == maxLength)
-                {
-                    length = len;
-                    return index;
-                }
-                v = value[len];
-            }
-            else if (len > 0)
-            {
-                v = value[0];
-                if (EqualityComparer<T>.Default.Equals(v, s))
-                {
-                    index = i;
-                    len = 1;
-                    v = value[1];
-                }
-                else
-                {
-                    index = -1;
-                    len = 0;
-                }
-            }
-        }
-        length = len;
-        return index;
-    }
 }
 
 public class EnumJsonConverterTest
---
+++ IT.Json/Internal/xReadOnlySpan.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace IT.Json.Internal;
+
+internal static class xReadOnlySpan
+{
+    public static int IndexOfPart<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> value, out int length)
+        where T : IEquatable<T>?
+    {
+        if (span.Length == 0) throw new ArgumentException("span is empty", nameof(span));
+
+        var maxLength = value.Length;
+        if (maxLength == 0) throw new ArgumentException("value is empty", nameof(value));
+        if (maxLength == 1)
+        {
+            var idx = span.IndexOf(value);
+            length = idx == -1 ? 0 : 1;
+            return idx;
+        }
+
+        var index = -1;
+        var len = 0;
+        var v = value[0];
+        for (int i = 0; i < span.Length; i++)
+        {
+            var s = span[i];
+            if (EqualityComparer<T>.Default.Equals(v, s))
+            {
+                if (index == -1) index = i;
+                if (++len == maxLength)
+                {
+                    length = len;
+                    return index;
+                }
+                v = value[len];
+            }
+            else if (len > 0)
+            {
+                v = value[0];
+                if (EqualityComparer<T>.Default.Equals(v, s))
+                {
+                    index = i;
+                    len = 1;
+                    v = value[1];
+                }
+                else
+                {
+                    index = -1;
+                    len = 0;
+                }
+            }
+        }
+        length = len;
+        return index;
+    }
+}
