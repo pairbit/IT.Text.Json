745319e36181cca559d6e4b32e2e19d36932040e
Ivan Tikhonov <itikhonov@mail.com>
2025-01-06 20:33:14 +0300

    byte[] flags

--- IT.Json.Benchmarks/FlagsEnumJsonConverterBenchmark.cs
+++ IT.Json.Benchmarks/FlagsEnumJsonConverterBenchmark.cs
@@ -22,7 +22,7 @@ public enum EnumByteFlags : byte
 public class FlagsEnumJsonConverterBenchmark
 {
     private static EnumByteFlags _maxFlags;
-    private static string _maxFlagsString = null!;
+    private static byte[] _maxFlagsString = null!;
 
     private static JsonSerializerOptions _jsoStringCamelCase = null!;
     private static JsonSerializerOptions _jsoStringCamelCaseInteger = null!;
@@ -45,16 +45,16 @@ public class FlagsEnumJsonConverterBenchmark
     }
 
     [Benchmark]
-    public string Serialize_Number() => JsonSerializer.Serialize(_maxFlags);
+    public byte[] Serialize_Number() => JsonSerializer.SerializeToUtf8Bytes(_maxFlags);
 
     [Benchmark]
-    public string Serialize_String() => JsonSerializer.Serialize(_maxFlags, _jsoStringCamelCase);
+    public byte[] Serialize_String() => JsonSerializer.SerializeToUtf8Bytes(_maxFlags, _jsoStringCamelCase);
 
     [Benchmark]
-    public string Serialize_Strict() => JsonSerializer.Serialize(_maxFlags, _jsoStringCamelCaseStrict);
-    
+    public byte[] Serialize_Strict() => JsonSerializer.SerializeToUtf8Bytes(_maxFlags, _jsoStringCamelCaseStrict);
+
     [Benchmark]
-    public EnumByteFlags Deserialize_Number() => JsonSerializer.Deserialize<EnumByteFlags>("15");
+    public EnumByteFlags Deserialize_Number() => JsonSerializer.Deserialize<EnumByteFlags>("15"u8);
 
     [Benchmark]
     public EnumByteFlags Deserialize_String() => JsonSerializer.Deserialize<EnumByteFlags>(_maxFlagsString, _jsoStringCamelCase);
@@ -66,9 +66,9 @@ public class FlagsEnumJsonConverterBenchmark
     {
         GlobalSetup();
 
-        if (Serialize_Number() != "15") throw new InvalidOperationException("Serialize_Number");
-        if (Serialize_String() != _maxFlagsString) throw new InvalidOperationException("Serialize_String");
-        if (Serialize_Strict() != _maxFlagsString) throw new InvalidOperationException("Serialize_Strict");
+        if (!Serialize_Number().AsSpan().SequenceEqual("15"u8)) throw new InvalidOperationException("Serialize_Number");
+        if (!Serialize_String().AsSpan().SequenceEqual(_maxFlagsString)) throw new InvalidOperationException("Serialize_String");
+        if (!Serialize_Strict().AsSpan().SequenceEqual(_maxFlagsString)) throw new InvalidOperationException("Serialize_Strict");
 
         if (Deserialize_Number() != _maxFlags) throw new InvalidOperationException("Deserialize_Number");
         if (Deserialize_String() != _maxFlags) throw new InvalidOperationException("Deserialize_String");
--- IT.Json.Benchmarks/Program.cs
+++ IT.Json.Benchmarks/Program.cs
@@ -2,16 +2,16 @@
 
 //new EnumJsonConverterBenchmark().Test();
 //new SequenceEnumJsonConverterBenchmark().Test();
-//new FlagsEnumJsonConverterBenchmark().Test();
-new SequenceFlagsEnumJsonConverterBenchmark().Test();
+new FlagsEnumJsonConverterBenchmark().Test();
+//new SequenceFlagsEnumJsonConverterBenchmark().Test();
 
 //new Base64JsonConverterBenchmark().Test();
 //new SequenceBase64JsonConverterBenchmark().Test();
 
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<EnumJsonConverterBenchmark>();
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<SequenceEnumJsonConverterBenchmark>();
-//BenchmarkDotNet.Running.BenchmarkRunner.Run<FlagsEnumJsonConverterBenchmark>();
-BenchmarkDotNet.Running.BenchmarkRunner.Run<SequenceFlagsEnumJsonConverterBenchmark>();
+BenchmarkDotNet.Running.BenchmarkRunner.Run<FlagsEnumJsonConverterBenchmark>();
+//BenchmarkDotNet.Running.BenchmarkRunner.Run<SequenceFlagsEnumJsonConverterBenchmark>();
 
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<Base64JsonConverterBenchmark>();
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<SequenceBase64JsonConverterBenchmark>();
