8898425110c748383d1d9e4dc510075c95b25cde
Ivan Tikhonov <itikhonov@mail.com>
2025-01-08 21:18:20 +0300

    refac

--- IT.Json/Converters/Base64JsonConverterFactory.cs
+++ IT.Json/Converters/Base64JsonConverterFactory.cs
@@ -8,30 +8,33 @@ namespace IT.Json.Converters;
 public class Base64JsonConverterFactory : JsonConverterFactory
 {
     private readonly int _maxEncodedLength;
+    private readonly MemoryPool<byte> _pool;
 
     public Base64JsonConverterFactory()
     {
         _maxEncodedLength = int.MaxValue;
+        _pool = MemoryPool<byte>.Shared;
     }
 
-    public Base64JsonConverterFactory(int maxEncodedLength)
+    public Base64JsonConverterFactory(int maxEncodedLength, MemoryPool<byte>? pool = null)
     {
         _maxEncodedLength = maxEncodedLength;
+        _pool = pool ?? MemoryPool<byte>.Shared;
     }
 
     public override bool CanConvert(Type type) =>
         type == typeof(byte[]) ||
         type == typeof(Memory<byte>) ||
         type == typeof(ReadOnlyMemory<byte>) ||
+        //type == typeof(ReadOnlySequence<byte>) ||
         type == typeof(IMemoryOwner<byte>);
-
 
     public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
         if (typeToConvert == typeof(byte[])) return new ArrayByteJsonConverter(_maxEncodedLength);
         if (typeToConvert == typeof(Memory<byte>)) return new MemoryByteJsonConverter(_maxEncodedLength);
         if (typeToConvert == typeof(ReadOnlyMemory<byte>)) return new ReadOnlyMemoryByteJsonConverter(_maxEncodedLength);
-        if (typeToConvert == typeof(IMemoryOwner<byte>)) return new MemoryOwnerByteJsonConverter(_maxEncodedLength);
+        if (typeToConvert == typeof(IMemoryOwner<byte>)) return new MemoryOwnerByteJsonConverter(_maxEncodedLength, _pool);
 
         throw new ArgumentOutOfRangeException(nameof(typeToConvert), typeToConvert, "Type not supported");
     }
