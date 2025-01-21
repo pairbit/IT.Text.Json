10b4abbf52204018b14247965cb8d67929af32e5
Ivan Tikhonov <itikhonov@mail.com>
2025-01-21 12:26:15 +0300

    ArrayPoolTest

---
+++ IT.Json.Tests/ArrayPoolTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Buffers;
+
+namespace IT.Json.Tests;
+
+public class ArrayPoolTest
+{
+    [Test]
+    public void Test()
+    {
+        var max = 1024 * 1024 * 1024;//1 GB
+        var rented = ArrayPool<byte>.Shared.Rent(max);
+
+        Assert.That(rented, Is.Not.Null);
+        Assert.That(rented, Has.Length.EqualTo(max));
+
+        ArrayPool<byte>.Shared.Return(rented);
+
+        rented = ArrayPool<byte>.Shared.Rent(Array.MaxLength);
+
+        Assert.That(rented, Is.Not.Null);
+        Assert.That(rented, Has.Length.EqualTo(Array.MaxLength));
+
+        ArrayPool<byte>.Shared.Return(rented);
+
+        //GC.AllocateUninitializedArray
+    }
+}
